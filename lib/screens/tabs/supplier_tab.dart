b473e5afe60a12a67dcb5a6275477de55c34743b
Lans12345 <olanalans12345@gmail.com>
2023-01-19 09:57:32 +0800

    added layout in first page

--- lib/screens/tabs/supplier_tab.dart
+++ lib/screens/tabs/supplier_tab.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:tracking_system/utils/colors.dart';
 import 'package:tracking_system/widgets/text_widget.dart';
 
-class SupplierTab extends StatelessWidget {
-  const SupplierTab({super.key});
+class SupplierTab extends StatefulWidget {
+  @override
+  State<SupplierTab> createState() => _SupplierTabState();
+}
+
+class _SupplierTabState extends State<SupplierTab> {
+  var count = 1;
+
+  var kinds = ['Original', 'Surplus', 'Replacement'];
+
+  var kind = '';
+
+  int dropDownValue = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -118,9 +129,53 @@ class SupplierTab extends StatelessWidget {
           ),
           Divider(),
           Padding(
-            padding: const EdgeInsets.only(left: 30),
-            child: TextBold(
-                text: 'Products Available', fontSize: 24, color: Colors.black),
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextBold(
+                    text: 'Products Available',
+                    fontSize: 24,
+                    color: Colors.black),
+                // MaterialButton(
+                //     child: TextRegular(
+                //         text: 'Add Product', fontSize: 12, color: Colors.white),
+                //     height: 40,
+                //     minWidth: 150,
+                //     color: Colors.blue[800],
+                //     onPressed: (() {
+                //       showModalBottomSheet(
+                //           context: context,
+                //           builder: ((context) {
+                //             return StatefulBuilder(
+                //                 builder: ((context, setState) {
+                //               return SizedBox(
+                //                 height: 500,
+                //                 child: Column(
+                //                   children: [
+                //                     Padding(
+                //                       padding: const EdgeInsets.only(right: 30),
+                //                       child: Align(
+                //                         alignment: Alignment.topRight,
+                //                         child: IconButton(
+                //                           onPressed: (() {
+                //                             Navigator.pop(context);
+                //                           }),
+                //                           icon: Icon(
+                //                             Icons.close,
+                //                             color: Colors.red,
+                //                           ),
+                //                         ),
+                //                       ),
+                //                     ),
+                //                   ],
+                //                 ),
+                //               );
+                //             }));
+                //           }));
+                //     })),
+              ],
+            ),
           ),
           SizedBox(
             height: 20,
@@ -259,7 +314,191 @@ class SupplierTab extends StatelessWidget {
                     );
                   })),
             ),
-          )
+          ),
+          Divider(),
+          Container(
+            width: double.infinity,
+            height: 180,
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 5),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Tooltip(
+                    message: 'Click this button to add item',
+                    child: MaterialButton(
+                        child: TextRegular(
+                            text: 'Add Item',
+                            fontSize: 12,
+                            color: Colors.white),
+                        color: primary,
+                        onPressed: (() {
+                          setState(() {
+                            count++;
+                          });
+                        })),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  VerticalDivider(),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  SingleChildScrollView(
+                    child: SizedBox(
+                      child: Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextRegular(
+                                  text: 'Item Description: ',
+                                  fontSize: 12,
+                                  color: Colors.black),
+                              for (int i = 1; i < count; i++)
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 5, bottom: 5),
+                                  child: SizedBox(
+                                    width: 220,
+                                    height: 40,
+                                    child: TextFormField(
+                                      decoration: InputDecoration(
+                                          fillColor: Colors.grey[300],
+                                          filled: true,
+                                          border: InputBorder.none),
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextRegular(
+                                  text: 'Item Price: ',
+                                  fontSize: 12,
+                                  color: Colors.black),
+                              for (int i = 1; i < count; i++)
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 5, bottom: 5),
+                                  child: SizedBox(
+                                    width: 100,
+                                    height: 40,
+                                    child: TextFormField(
+                                      decoration: InputDecoration(
+                                          fillColor: Colors.grey[300],
+                                          filled: true,
+                                          border: InputBorder.none),
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextRegular(
+                                  text: 'Quantity: ',
+                                  fontSize: 12,
+                                  color: Colors.black),
+                              for (int i = 1; i < count; i++)
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 5, bottom: 5),
+                                  child: SizedBox(
+                                    width: 100,
+                                    height: 40,
+                                    child: TextFormField(
+                                      decoration: InputDecoration(
+                                          fillColor: Colors.grey[300],
+                                          filled: true,
+                                          border: InputBorder.none),
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextRegular(
+                                  text: 'Kind:',
+                                  fontSize: 12,
+                                  color: Colors.black),
+                              for (int i = 1; i < count; i++)
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 5, bottom: 5),
+                                  child: SizedBox(
+                                    width: 100,
+                                    height: 40,
+                                    child: TextFormField(
+                                      decoration: InputDecoration(
+                                          fillColor: Colors.grey[300],
+                                          filled: true,
+                                          border: InputBorder.none),
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  VerticalDivider(),
+                  count != 1
+                      ? Padding(
+                          padding: const EdgeInsets.fromLTRB(30, 0, 30, 50),
+                          child: Center(
+                            child: Align(
+                              alignment: Alignment.bottomCenter,
+                              child: Tooltip(
+                                message: 'Click this button to post all items',
+                                child: MaterialButton(
+                                    minWidth: 180,
+                                    height: 50,
+                                    child: TextRegular(
+                                        text: 'Post Item',
+                                        fontSize: 18,
+                                        color: Colors.white),
+                                    color: Color(0xff4EA430),
+                                    onPressed: (() {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(
+                                        SnackBar(
+                                          backgroundColor: Colors.white,
+                                          content: TextRegular(
+                                              text: 'Items posted succesfully!',
+                                              fontSize: 18,
+                                              color: Colors.black),
+                                        ),
+                                      );
+                                    })),
+                              ),
+                            ),
+                          ),
+                        )
+                      : SizedBox(),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
